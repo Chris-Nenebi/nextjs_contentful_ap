975d7acaeb69ebf1c1fbe89a6817d1b61e009e44
Springer <kwametetth33@yahoo.com>
2021-04-16 11:48:19 +0000

    initial

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -1,7 +1,105 @@
-export default function RecipeDetails() {
+import Image from 'next/image'
+
+import {createClient} from "contentful"
+
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
+
+
+ const client = createClient({
+  space:process.env.CONTENTFUL_SPACE_ID,
+  accessToken:process.env.CONTENTFUL_ACCESS_KEY
+})
+
+export const getStaticPaths = async () =>{
+  const res = await client.getEntries({
+    content_type:"recipe"
+  })
+
+  const paths = res.items.map(item=>{
+    return {
+      params:{slug:item.fields.slug}
+    }
+  })
+
+  return {
+    paths:paths,
+    fallback:false
+  }
+}
+
+export  async function getStaticProps(context){
+  const params = context.params
+
+  const res = await client.getEntries({
+    content_type:"recipe",
+    "fields.slug":params.slug
+  })
+
+  // res is an array even though it returns only 1 item
+  return {
+    props:{
+      recipe:res.items[0]
+    }
+  }
+}
+
+export default function RecipeDetails({recipe}) {
+
+  const {featuredImage,title,method,cookingTime,ingredients} = recipe.fields
+
+  console.log(title)
   return (
     <div>
-      Recipe Details
+        <div className="banner">
+           <Image src={"https:" + featuredImage.fields.file.url}
+           width={featuredImage.fields.file.details.image.width}
+           height={featuredImage.fields.file.details.image.height}
+           />
+          <h2>{title}</h2>
+        </div>
+        <div className="info">
+          <p>Takes about {cookingTime} mins to cook.</p>
+          <h3>Ingredients: </h3>
+          {ingredients.map(ing =>(
+            <span key={ing}>{ing}</span>
+          ))}
+        </div>
+        <div className="method">
+          <h3>Method:</h3>
+          <div>{documentToReactComponents(method)}</div>
+        </div>
+
+      <style jsx>{`
+
+      h2,h3 {
+          text-transform: uppercase;
+        }
+
+        .banner h2 {
+          margin: 0;
+          background: #fff;
+          display: inline-block;
+          padding: 20px;
+          position: relative;
+          top: -60px;
+          left: -10px;
+          transform: rotateZ(-1deg);
+          box-shadow: 1px 3px 5px rgba(0,0,0,0.1);
+        }
+
+        .info p {
+          margin: 0;
+        }
+
+        .info span::after {
+          content: ", ";
+        }
+
+        .info span:last-child::after {
+          content: ".";
+        }
+
+       `}</style>
     </div>
   )
 }
